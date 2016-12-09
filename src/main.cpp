9d36ce73322d99b88c5b1f3ada991fcd1aa73fb7
NivenT <nachenjang@gmail.com>
2016-12-09 15:06:59 -0800

    About to try to switch from savevideos -> convert2mp3 (should maybe have just created a separate branch but meh)

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,10 @@
 
 using json = nlohmann::json;
 
+bool starts_with(const std::string& str, const std::string& prefix) {
+	return str.find(prefix) == 0;
+}
+
 bool ends_with(const std::string& str, const std::string& suffix) {
 	return str.find_last_of(suffix) == str.size() - 1;
 }
@@ -21,20 +25,41 @@ void write_to_mp3(const std::string& title, const std::string& data) {
 
 std::string youtube_to_download(const std::string& id) {
 	std::string url = "http://savevideos.xyz/api?v=" + id;
-	auto html = cpr::Get(cpr::Url{url});
 
-	std::cout<<"url: "<<url<<std::endl
-		 	 <<"status code: "<<html.status_code<<std::endl
-			 <<"HTML Response:"<<std::endl
-			 <<html.text;
-	return html.text;
+	auto response = cpr::Get(cpr::Url{url});
+	auto json = json::parse(response.text);
+	/*
+	std::cout<<"response:"<<std::endl
+			 <<json.dump(4)<<std::endl
+			 <<std::endl;
+	*/
+	return json["audio"][0]["url"];
+}
+
+std::string to_http(const std::string& url) {
+	return starts_with(url, "https") ? "http" + url.substr(5) :
+		   starts_with(url, "http")  ? url : 
+		   							   "http" + url;
+}
+
+void download_song(const std::string& url) {
+	auto response = cpr::Get(cpr::Url{url});
+	//auto json = json::parse(response.text);
+	/*
+	std::cout<<"url 2: "<<url<<std::endl
+			 <<"response 2:"<<std::endl
+			 <<response.text<<std::endl
+			 <<std::endl;
+	*/
+	std::cout<<response.text;
 }
 
 int main(int argc, char** argv) {
 	const std::string christmas = "http://dl3.downloader.space/dl.php?id=e506ebfd91227183a20f223ec8479998";
 	const std::string christmas2 = "pFjdfjrtf1Q";
 
-	youtube_to_download(christmas2);
+	const std::string downloadUrl = youtube_to_download(christmas2);
+	download_song(to_http(downloadUrl));
 	/*
     auto response = cpr::Get(cpr::Url{christmas});
     write_to_mp3("Thats_Christmas_To_Me.mp3", response.text);
