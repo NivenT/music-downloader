15baf811256ef5c43fbe8c15a694f618d1053df2
NivenT <nachenjang@gmail.com>
2020-06-12 14:51:04 -0400

    added cout<<endl;

--- src/main.cpp
+++ src/main.cpp
@@ -141,6 +141,7 @@ int main(int argc, const char** argv) {
         for (const auto& id : yt_ids) {
             string title = get_title_from_id(id, verbose);
             download_song_given_id(id, title, saveFolder, verbose, stats);
+            cout<<endl;
         }
         print_statistics(stats);
     } else if (!(yt_ids = args["--play-from-ids"].asStringList()).empty()) {
